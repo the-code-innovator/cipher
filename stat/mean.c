2214b0e2d26b5a15925612317ee6bf724416f536
Aravind M <the.code.innovator@gmail.com>
2019-05-15 10:56:53 +0530

    changes

--- stat/mean.c
+++ stat/mean.c
@@ -1,10 +1,14 @@
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 int main(int argc, char *argv[]) {
-	// size_t array_count = 0UL;
-	// printf("%zu\n", array_count);
-	
+	if (argc >= 2UL) {
+		size_t array_count = 0UL;
+		for (size_t iterator = 1UL; iterator < argc; iterator += 1UL) {
+			
+		}
+	}
 	return 0;
 }
