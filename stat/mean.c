8b00692e018d003c6d021145015873fd6e0f8532
Aravind M <the.code.innovator@gmail.com>
2019-05-15 10:39:22 +0530

    changes

---
+++ stat/mean.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+
+int main(int argc, char *argv[]) {
+	// size_t array_count = 0UL;
+	// printf("%zu\n", array_count);
+	
+	return 0;
+}
